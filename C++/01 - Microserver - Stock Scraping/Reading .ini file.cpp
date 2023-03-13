f8cbbff144f9a848c6fc7e7b7a6e5a35d1cabe5e
Andre Pinheiro <apinheiro99@gmail.com>
2023-03-12 22:31:25 -0400

    Research - reading a .ini file

---
+++ C++/01 - Microserver - Stock Scraping/Reading .ini file.cpp
@@ -0,0 +1,2 @@
+// You can use the Windows API functions, such as GetPrivateProfileString() and GetPrivateProfileInt().
+// https://stackoverflow.com/questions/12633/what-is-the-easiest-way-to-parse-an-ini-file-in-c
